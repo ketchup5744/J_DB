bd6a28b2e1b5ed3852f955cbbadc65dabfd1f24d
Josh <vkd5744@naver.com>
2021-05-21 17:31:29 +0900

    J-DB opening commit

---
+++ src/com/J/DB/DB.java
@@ -0,0 +1,11 @@
+package com.J.DB;
+
+public class DB {
+
+	public static void main(String[] args) {
+		TeaType tea = TeaType.보성녹차;
+		System.out.println("J 다방에 오신걸 환영합니다");
+		System.out.println(TeaType.율무차);
+	}
+
+}
---
+++ src/com/J/DB/TeaType.java
@@ -0,0 +1,5 @@
+package com.J.DB;
+
+public enum TeaType {
+	감잎차, 보성녹차, 율무차
+}
